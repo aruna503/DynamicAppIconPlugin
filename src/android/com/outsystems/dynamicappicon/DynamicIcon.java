8265e92985809a95187598837a9390b4ea898af8
aruna503 <44997651+aruna503@users.noreply.github.com>
2025-12-11 15:20:44 +0530

    Update DynamicIcon.java

--- src/android/com/outsystems/dynamicappicon/DynamicIcon.java
+++ src/android/com/outsystems/dynamicappicon/DynamicIcon.java
@@ -13,18 +13,20 @@ public class DynamicIcon extends CordovaPlugin {
  
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+ 
         if ("changeIcon".equals(action)) {
  
             String iconName = args.getString(0);
-            changeIcon(iconName);
+            switchIcon(iconName);
  
             callbackContext.success("Icon switch executed");
             return true;
         }
+ 
         return false;
     }
  
-    private void changeIcon(String iconName) {
+    private void switchIcon(String iconName) {
  
         Context context = cordova.getActivity().getApplicationContext();
         PackageManager pm = context.getPackageManager();
@@ -36,22 +38,18 @@ public class DynamicIcon extends CordovaPlugin {
         ComponentName privateIcon = new ComponentName(pkg, pkg + ".IconPrivate");
  
         pm.setComponentEnabledSetting(normal,
-                iconName.equalsIgnoreCase("normal")
-                        ? PackageManager.COMPONENT_ENABLED_STATE_ENABLED
-                        : PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+                iconName.equalsIgnoreCase("normal") ? PackageManager.COMPONENT_ENABLED_STATE_ENABLED :
+                        PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
                 PackageManager.DONT_KILL_APP);
  
         pm.setComponentEnabledSetting(premium,
-                iconName.equalsIgnoreCase("premium")
-                        ? PackageManager.COMPONENT_ENABLED_STATE_ENABLED
-                        : PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+                iconName.equalsIgnoreCase("premium") ? PackageManager.COMPONENT_ENABLED_STATE_ENABLED :
+                        PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
                 PackageManager.DONT_KILL_APP);
  
         pm.setComponentEnabledSetting(privateIcon,
-                iconName.equalsIgnoreCase("private")
-                        ? PackageManager.COMPONENT_ENABLED_STATE_ENABLED
-                        : PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+                iconName.equalsIgnoreCase("private") ? PackageManager.COMPONENT_ENABLED_STATE_ENABLED :
+                        PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
                 PackageManager.DONT_KILL_APP);
     }
 }
- 
